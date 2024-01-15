a497535dceb46cd1f54dc8c1e3554d30346e06bd
alejandromotta <alejandromotta808@gmail.com>
2024-01-15 15:32:58 -0500

    test

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  const htmlResponse = `
-    <html>
-      <head>
-        <title>NodeJs y Express en Vercel</title>
-      </head>
-      <body>
-        <h1>Soy un proyecto Back end en vercel</h1>
-      </body>
-    </html>
-  `;
-  res.send(htmlResponse);
-});
+// app.get("/", (req, res) => {
+//   const htmlResponse = `
+//     <html>
+//       <head>
+//         <title>NodeJs y Express en Vercel</title>
+//       </head>
+//       <body>
+//         <h1>Soy un proyecto Back end en vercel</h1>
+//       </body>
+//     </html>
+//   `;
+//   res.send(htmlResponse);
+// });
 
-app.listen(port, () => {
-  console.log(`port runing in http://localhost:${port}`);
-}); 
+// app.listen(port, () => {
+//   console.log(`port runing in http://localhost:${port}`);
+// }); 
+
+import { sql } from "@vercel/postgres";
+
+export default async function Cart({ params }) {
+  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+
+  return (
+    <div>
+      ALO
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.quantity}
+        </div>
+      ))}
+    </div>
+  );
+}
